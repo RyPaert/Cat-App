1003493c5c1b19b46c47d20f1cc5aea7622a1e00
Opilane TTHK <158561974+RyPaert@users.noreply.github.com>
2025-09-15 10:47:19 +0300

    Registration Model
    Made a model for user registration, it includes error messages.

---
+++ Models/Accounts/UserRegistrationModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catblog.Models.Accounts
+{
+    public class UserRegistrationModel
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "a Password is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "an Email is required!")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "the Passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
