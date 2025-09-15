4c493f64b04d8fa4f2ea8810b148d11ccb45e1e7
Opilane TTHK <158561974+RyPaert@users.noreply.github.com>
2025-09-15 11:20:46 +0300

    Necessary nugets & Base for AccountsController
    >Adds Identity.Core
    >Empty base for AccountsController
    
    for future reference this link here is my guide: https://code-maze.com/user-registration-aspnet-core-identity/

---
+++ Controllers/Accounts/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using Catblog.Models.Accounts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catblog.Controllers.Accounts
+{
+    public class AccountsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserRegistrationModel userModel)
+        {
+            return View();
+        }
+    }
+}
