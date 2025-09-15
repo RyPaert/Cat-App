403ff197397f31fb9b0784c6f52dea3c0da457a9
opilane <opilane@HP-CZC321706M>
2025-09-15 11:14:39 +0300

    Create admin model- create, delete and index

---
+++ Models/KittysAdmin/AdminKittyCreate.cs
@@ -0,0 +1,13 @@
+﻿namespace Catblog.Models.KittysAdmin
+{
+    public class AdminKittyCreate
+    {
+        public int AdminCatId;
+        public string AdminCatName;
+        public string AdminCatSpecies;
+        public int AdminCatAge;
+        public string AdminCatGender;
+        public string AdminCatDescription;
+        public int AdminCatRate;
+    }
+}
