bb86871d9a0ed465e04a1f6fb04c191e381c1d76
opilane <opilane@HP-CZC321706M>
2025-09-19 11:21:11 +0300

    View ja controller creates

---
+++ Controllers/AdminKittysController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Catblog.Models.KittysAdmin;
+
+
+namespace Catblog.Controllers
+{
+    public class AdminKittysController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AdminCatId, AdminCatName, AdminCatSpecies, AdminCatGender, AdminCatDescription")]AdminKittyCreate adminKittys)
+        {
+            
+        }
+    }
+}
--- Models/KittysAdmin/AdminKittyCreate.cs
+++ Models/KittysAdmin/AdminKittyCreate.cs
@@ -2,12 +2,13 @@
 {
     public class AdminKittyCreate
     {
-        public int AdminCatId;
-        public string AdminCatName;
-        public string AdminCatSpecies;
-        public int AdminCatAge;
-        public string AdminCatGender;
-        public string AdminCatDescription;
-        public int AdminCatRate;
+        public int AdminCatId {  get; set; }
+        public string AdminCatName { get; set; }
+        public string AdminCatSpecies { get; set; }
+        public int AdminCatAge { get; set; }
+        public string AdminCatGender { get; set; }
+        public string AdminCatDescription { get; set; }
+        public int AdminCatRate { get; set; }
+        public AdminKittyCreate adminKittyCreate { get; set; }
     }
 }
