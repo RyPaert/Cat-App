d75a13a3652d46d5592d0e68b100922633b2a21c
RyPaert <158561974+RyPaert@users.noreply.github.com>
2025-09-29 10:04:58 +0300

    login model

---
+++ Models/Accounts/UserLoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catblog.Models.Accounts
+{
+    public class UserLoginModel
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "a Password is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "an Email is required!")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "the Passwords do not match!")]
+        [Required(ErrorMessage = "Password confirmation is required!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
