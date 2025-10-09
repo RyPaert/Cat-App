83ea1806841e8915d13ddbb17b721f6dcb138d37
opilane <opilane@LENOVO-PF260C6A>
2025-10-09 14:39:22 +0300

    Find url to create comment fucking function now! https://stackoverflow.com/questions/70612483/creating-a-comment-like-box-in-asp-net-core

--- Models/Post/Comment.cs
+++ Models/Post/Comment.cs
@@ -10,5 +10,6 @@ namespace Catblog.Models.Post
         public string? Text { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public Guid? PostId { get; set; }
+        //https://stackoverflow.com/questions/70612483/creating-a-comment-like-box-in-asp-net-core
     }
 }
