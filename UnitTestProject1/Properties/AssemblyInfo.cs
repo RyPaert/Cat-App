3bb7b25dcbb8a2cb7401b8bb691c65f34d0035c4
Opilane TTHK <158561974+RyPaert@users.noreply.github.com>
2025-11-05 11:13:51 +0200

    blah

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TTHK")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © TTHK 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c194d65-8729-48aa-83eb-be214bfae336")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UserServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Catblog.ServiceInterface;
+using Catblog.Services;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class UserServiceTests
+    {
+        PostServices postService;
+        IPostServices ipostService;
+
+        [SetUp]
+        public void Setup()
+        {
+            postService = new PostServices();
+        }
+
+        [Test]
+        public void PostDetailsAsync_Works()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
