d0520d9144eb9a9f22772ab2e558d433bd9f4b2b
WADWADADAEDA <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-11-27 17:16:43 +0200

    Add Some File

---
+++ Catblog/TestProject/Mock/MockIHostEnvironment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Mock
+{
+    public class MockIHostEnvironment : IWebHost
+    {
+        public string EnvironmentName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string ApplicationName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string ContentRootPath
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public IFileProvider ContentRootFileProvider
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public IFeatureCollection ServerFeatures => throw new NotImplementedException();
+
+        public IServiceProvider Services => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Catblog/TestProject/TestBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catblog.Data;
+using Catblog.ServiceInterface;
+using Catblog.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using TestProject.Macros;
+using TestProject.Mock;
+
+namespace TestProject
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IPostServices, PostServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IWebHost, MockIHostEnvironment>();
+
+            services.AddDbContext<CatblogDb>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            }
+            );
+
+            RegisterMacros(services);
+        }
+        public void Dispose() { }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+            var macros = macroBaseType.Assembly.GetTypes().Where(t => macroBaseType.IsAssignableFrom(t)
+            && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros) { services.AddSingleton(macro); }
+        }
+    }
+}
